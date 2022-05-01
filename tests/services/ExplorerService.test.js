f1c03c2cdcc8d19c4ed37e4f0c0f26fc66a18cfd
Marco <macdev.gmail@gmail.com>
2022-05-01 00:20:23 -0500

    Added a test for Explorer Service that test the methos getAmountOfExplorers by mission and make it fail on purpose

--- tests/services/ExplorerService.test.js
+++ tests/services/ExplorerService.test.js
@@ -50,4 +50,52 @@ describe("Unit test for ExplorerService", ()=>{
     expect(explorersInNode[1].mission).toBe("node")
     expect(explorersInNode[2].mission).toBe("node")
   });
+  test('2. Get amount of explorers by mission', ()=>{
+    const explorers = [
+      {
+        name: "Woopa11",
+        githubUsername: "ajolonauta11",
+        score: 11,
+        mission: "node",
+        stacks: [
+          "javascript",
+          "elixir",
+          "groovy",
+          "reasonML",
+          "elm"
+        ]
+      },
+      {
+        name: "Woopa12",
+        githubUsername: "ajolonauta12",
+        score: 12,
+        mission: "node",
+        stacks: [
+          "javascript",
+          "elixir",
+          "groovy",
+          "reasonML",
+          "elm"
+        ]
+      },
+      {
+        name: "Woopa13",
+        githubUsername: "ajolonauta13",
+        score: 13,
+        mission: "node",
+        stacks: [
+          "javascript",
+          "elixir",
+          "groovy",
+          "reasonML",
+          "elm"
+        ]
+      }
+    ]
+    
+    const explorersAmountByMission = ExplorerService.getAmountOfExplorersByMission(explorers, "node")
+    
+    expect(explorersAmountByMission).toBe(3)
+    
+  });
 })
