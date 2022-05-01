8cf88fc30f52ebdf73cdb2e4f8deb25d34648e8b
Marco <macdev.gmail@gmail.com>
2022-05-01 17:22:40 -0500

    buzz test created and failedit on purpose

--- tests/services/FizzbuzzService.test.js
+++ tests/services/FizzbuzzService.test.js
@@ -21,4 +21,24 @@ describe("Unit test for FizzbuzzService", ()=>{
     
     expect(fizzExplorer.trick).toBe("FIZZ")
   });
+  test('2. Validate Buzz', ()=>{
+    const explorer = {
+        name: "Woopa11",
+        githubUsername: "ajolonauta1",
+        score: 3,
+        mission: "node",
+        stacks: [
+          "javascript",
+          "elixir",
+          "groovy",
+          "reasonML",
+          "elm"
+        ],
+        trick: ""
+      }
+    
+    const fizzExplorer = FizzbuzzService.applyValidationInExplorer(explorer)
+    
+    expect(fizzExplorer.trick).toBe("BUZZ")
+  });
 })
