e6f251ef46c8953bcf15267248bc0019f6e7a927
Marco <macdev.gmail@gmail.com>
2022-05-02 00:02:21 -0500

    first test for explorer controller added and make it fail on purpose

---
+++ tests/controller/ExplorerController.test.js
@@ -0,0 +1,19 @@
+const ExplorerController = require("../../lib/controllers/ExplorerController");
+describe("Unit test for ExplorerController", ()=>{
+    test("1. Get explorer by mission", ()=>{
+        const explorersByMission = ExplorerController.getExplorersByMission("node");
+    
+        expect(explorersByMission[0]).toBe({
+            "name": "Woopa1",
+            "githubUsername": "ajolonauta1",
+            "score": 1,
+            "mission": "node",
+            "stacks": [
+              "javascript",
+              "reasonML",
+              "elm"
+            ]
+          });
+    });
+   
+});
